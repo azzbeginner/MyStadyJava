60ff4410714a30b0d265f65edfd968e94dbdc6db
aziz <azihat1985@gmail.com>
2025-05-12 14:17:20 +0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 import java.util.function.Function;
 
 public class Main {
+public static void main(String[] args) {
+Percon id1 = new Percon("Aziz", 40);
+Percon id2 = new Percon("Oleg", 42);
 
-
-    public static void main(String[] args) {
+    id1.PrintPerc();
+    id2.PrintPerc();
 
 
 }}
+
+class Percon {
+
+    private String name;
+    private int age;
+
+    Percon(String name,int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void PrintPerc () {
+        System.out.println(name + " " + age);
+    }
+}
