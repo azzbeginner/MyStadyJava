ec00a7a3a3b0b09cf1baed3fb7fe95376c9cd401
aziz <azihat1985@gmail.com>
2025-05-12 14:31:45 +0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ import java.util.function.Function;
 
 public class Main {
 public static void main(String[] args) {
-Percon id1 = new Percon("Aziz", 40);
-Percon id2 = new Percon("Oleg", 42);
 
+    System.out.println(Math.pow(3,20));
+    System.out.println(Percon.POW(3,20));
+
+
+    Percon id1 = new Percon("Aziz", 40);
+    Percon id2 = new Percon("Oleg", 42);
+    Percon.Status = "People";
     id1.PrintPerc();
     id2.PrintPerc();
 
@@ -15,12 +20,16 @@ class Percon {
 
     private String name;
     private int age;
+    static String Status = "Russians";
 
     Percon(String name,int age) {
         this.name = name;
         this.age = age;
     }
     public void PrintPerc () {
-        System.out.println(name + " " + age);
+        System.out.println(name + " " + age +  " " + Status);
+    }
+    public static double POW(double x,double y){
+        return Math.pow(x, y);
     }
 }
