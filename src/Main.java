2eef2397f216bac89bc75a820084ed7470d6d3dc
aziz <azihat1985@gmail.com>
2025-05-12 13:56:06 +0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 
 public class Main {
+
+
     public static void main(String[] args) {
-     
+        System.out.println(integralKvadret(0,2,x -> {
+            return Math.pow(x,2);
+        }));
     }
+    public static double integralKvadret(double a, double b, Function function){
+        double area = 0;
+        double h = 0.01;
+
+        for (int i = 0; i < (b-a)/h; i++) {area += h * function.funk( a + i * h);
+        }
+        return area;
+
+}}
+
+interface Function {
+    public double funk(double x);
+
+
 }
