10da8eb26766dfddb58113357b8c08dd8872a2e5
aziz <azihat1985@gmail.com>
2025-05-12 14:07:34 +0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(integralKvadret(0,2,x -> {return Math.pow(x,2);}));
-        System.out.println(integralKvadret(0,2,x -> {return Math.pow(x,3);}));
-
-    }
-    public static double integralKvadret(double a, double b, Function function) {
-        double area = 0;
-        double h = 0.01;
-        for (int i = 0; i < (b - a) / h; i++) {
-            area += h * function.funk(a + i * h);
-        }
-        return area;
-    }
-
-        public static double integralTrapec (double a, double b, Function function) {
-            double area = 0;
-            double h = 0.01;
-            for (int i = 0; i < (b - a) / h; i++) {
-                area += h * (0.5 * (function.funk(a + i * h) + function.funk(a + (i + 1) * h)));
-            }
-        return area;
-}
-
-interface Function {
-    public double funk(double x);
 
 
 }}
